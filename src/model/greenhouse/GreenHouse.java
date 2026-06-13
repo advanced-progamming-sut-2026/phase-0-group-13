2d611bdcfca307f84fb87951364218be1f4d0f40
amirhossein <bazdaramir08@gmail.com>
2026-06-13 16:36:29 +0330

    updating ShopItems

--- src/model/greenhouse/GreenHouse.java
+++ src/model/greenhouse/GreenHouse.java
@@ -1,7 +1,11 @@
 package model.greenhouse;
 
+import model.plant.BasePlant;
+
 public class GreenHouse {
     private int capacity;
+    private int time ;
+    private PlantUpgrade plantUpgrade;
 
     public GreenHouse() {
         this(0);
@@ -10,10 +14,21 @@ public class GreenHouse {
     public GreenHouse(int capacity) {
         this.capacity = capacity;
     }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {}
+
+    public void setTime(int time) {
+        this.time = time;
+    }
 
     public void showAllPlants() {
     }
 
     public void collectPlant() {
+    }
+    public void PlantMaking(BasePlant plant){
+        // اینجا میایم گیاه جدید و میزاریم ( کلاس پلنت اپگرید هم هستش )
     }
 }
--- src/model/shop/Shop.java
+++ src/model/shop/Shop.java
@@ -1,9 +1,21 @@
 package model.shop;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Shop {
+    private List<Objects> AllTimeProducts ;
+    private List<Objects> DailyTimeProducts ;
     public void listItems() {
     }
 
     public void buyItem(String itemId, int count, String plantType) {
     }
+    public List<Objects> getAllTimeProducts() {
+        return AllTimeProducts;
+    }
+    public void AddAllTimeProducts() {}
+    public void AddDailyProducts() {}
+
+
 }
