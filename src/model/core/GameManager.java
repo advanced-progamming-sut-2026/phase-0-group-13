e79827080e7fb899ed98a439400cca7fc6bce0e6
amirhossein <bazdaramir08@gmail.com>
2026-06-13 16:42:21 +0330

    adding news

--- src/model/core/GameManager.java
+++ src/model/core/GameManager.java
@@ -1,18 +1,23 @@
 package model.core;
 
 import model.game.Board;
+import model.news.AllNews;
+
+import java.sql.Array;
 
 public class GameManager {
     private Board board;
     private int sunAmount;
     private int plantFoodCount;
     private boolean running;
+    private AllNews allnews;
 
     public GameManager() {
         this.board = null;
         this.sunAmount = 0;
         this.plantFoodCount = 0;
         this.running = false;
+        this.allnews = new AllNews(null);
     }
 
     public GameManager(Board board) {
---
+++ src/model/news/news.java
@@ -0,0 +1,7 @@
+package model.news;
+
+public class news {
+    private boolean isRead ;
+    news(boolean isRead) {this.isRead = isRead;}
+    public boolean isRead() {return isRead;}
+}
